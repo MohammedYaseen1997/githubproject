5993d8622218fb9d65a726525608c2bb298a7d19
YASEEN <YASEEN>
2023-04-26 15:34:32 +0530

    all files

--- githubproject/src/githubproject/Githubproject.java
+++ githubproject/src/githubproject/Githubproject.java
@@ -1,10 +1,27 @@
 package githubproject;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class Githubproject {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	@Test
+	public void project() {
+	System.setProperty("webdriver.gecko.driver","C:\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+	WebDriver driver=new FirefoxDriver();
+	 driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	driver.get("http://localhost/orangehrm/orangehrm-5.2/orangehrm-5.2/web/index.php/auth/login");
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")).sendKeys("Yaseen");
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")).sendKeys("Yaseenstc@2022");
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")).click();
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[3]/a")).click();
+	driver.findElement(By.name("firstName")).sendKeys("Mohammed");
+	driver.findElement(By.name("lastName")).sendKeys("Yaseen");
+	driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]")).click();
+}
 }
